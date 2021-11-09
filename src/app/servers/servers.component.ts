8858bc6afe21395bcfc0d73971a5b417c303093a
zainuleb <p166024@nu.edu.pk>
2021-11-10 00:02:04 +0500

    Completed Property Binding with Button disbale value

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,11 +2,17 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: './servers.component.html',
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent implements OnInit {
-  constructor() {}
+  allowNewServer = false;
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit(): void {}
 }
