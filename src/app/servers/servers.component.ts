06b34871f613ab78c47c5c422afcda5c49cfe476
zainuleb <p166024@nu.edu.pk>
2021-11-10 00:16:15 +0500

    Completed Event Binding by changing state of a variable through button click

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -7,12 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
+  serverCreationStatus = 'No Server Was Created';
 
-  constructor() {
-    setTimeout(() => {
-      this.allowNewServer = true;
-    }, 2000);
-  }
+  constructor() {}
 
   ngOnInit(): void {}
+
+  // Function to change state of serverCreation Status
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was Created';
+  }
 }
