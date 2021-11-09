c1dbfc04bbf2d39503841d259235b2bb0742cc88
zainuleb <p166024@nu.edu.pk>
2021-11-10 01:28:20 +0500

    Used Directives *ngIf

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -9,14 +9,16 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No Server Was Created';
   serverName = '';
-
+  serverCreated = false;
   constructor() {}
 
   ngOnInit(): void {}
 
   // Function to change state of serverCreation Status
   onCreateServer() {
-    this.serverCreationStatus = 'Server was Created';
+    this.serverCreated = true;
+    this.serverCreationStatus =
+      'Server was Created! Name is ' + this.serverName;
   }
 
   onUpdateServerName(event: Event) {
